5963dfc113b5f040944c2c2b3813400512542e49
enjanurr <bajentingjanurr@gmail.com>
2024-04-23 10:00:13 +0800

    Merge remote-tracking branch 'origin/main'

---
+++ page3/indexpage3.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("redirectbutton").addEventListener("click",function(){
+        window.location.href= "/page2/indexpg2.html";
+    });  
+});
