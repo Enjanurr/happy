72f39dbb753cc81ffbb9cdf47cfb501a02e8939d
enjanurr <bajentingjanurr@gmail.com>
2024-04-23 22:46:40 +0800

    delete

--- web/page3/indexpage3.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("redirectbutton").addEventListener("click",function(){
-        window.location.href= "/page2/indexpg2.html";
-    });  
-});
